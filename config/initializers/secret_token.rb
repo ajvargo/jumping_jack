6c6361bb83528551adc3f0de80bf7e80e15a84b7
Andrew J Vargo <ajvargo@computer.org>
2010-10-16 11:25:41 -0400

    New project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JumpingJacks::Application.config.secret_token = '6d9c7666c2af57bac5ccd5d2767b48122f760ad0c5f54e7978906e12e2f8ec161ec09e9bc12b0b19bbed7d5cf29b0bc16eb0e3ea688fb6b67acccdb80585b067'
