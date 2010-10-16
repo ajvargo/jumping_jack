efe251bb3861d4314a2486c52455c4182a8168cf
Andrew J Vargo <ajvargo@computer.org>
2010-10-16 17:24:13 -0400

    Added validations to Trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,4 @@
 class Trip < ActiveRecord::Base
+  validates :day, :presence => true, :date => true
+  validates :duration, :presence => true, :numericality => {:integer_only => true, :greater_than => 0}
 end
